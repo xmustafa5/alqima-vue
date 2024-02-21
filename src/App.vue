fc89f1fcb41a5ea31cbd19ed4d31f742306134a8
Mustafa mohammed <107437940+xmustafa5@users.noreply.github.com>
2024-02-21 10:36:27 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Table from './components/Table.vue';
+
+</script>
+
+<template>
+  <Table title="mustafa" age=20 />
+</template>
+
+
---
+++ src/components/Table.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps(['title','age'])
+</script>
+
+<template>
+<div class=" w-full h-screen ">
+    <div class='bg-red-500'>
+        <h1 class="text-2xl font-bold ">اسم الموضف</h1>
+    </div>
+</div>
+</template>
